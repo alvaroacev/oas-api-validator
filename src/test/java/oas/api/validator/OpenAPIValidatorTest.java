6ca7a793c781a89f6ee80843af8c5269737d8231
Alvaro Acevedo <acevedo.alvaro@gmail.com>
2019-12-29 00:47:03 +0000

    cosmetic changes

--- src/test/java/oas/api/validator/OpenAPIValidatorTest.java
+++ src/test/java/oas/api/validator/OpenAPIValidatorTest.java
@@ -6,8 +6,8 @@ public class OpenAPIValidatorTest {
 
 	@Test
 	public void testMain() {
-		OpenAPIValidator.main(new String[] { "-oas", "/Users/alface/Documents/development/sources/oas-api-validator/src/test/resources/oas/v3/api-gpi-v3_0_11-SDK.yaml", //
-				"-response", "/Users/alface/Documents/development/sources/oas-api-validator/src/test/resources/responses/payment_transaction_details_serial.json", // 
+		OpenAPIValidator.main(new String[] { "-oas", "{path}/oas-api-validator/src/test/resources/oas/v3/api-gpi-v3_0_11-SDK.yaml", //
+				"-response", "{path}/oas-api-validator/src/test/resources/responses/payment_transaction_details_serial.json", // 
 				"-apiPath", "/payments/{uetr}/transactions", // 
 				"-httpMethod", "GET" });
 
